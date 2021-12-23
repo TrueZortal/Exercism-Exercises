df6b9b05553306dbc711d6440f331d4b02014e46
Mateusz Buczek <mtsbck@gmail.com>
2021-12-24 00:57:14 +0100

    v1

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,18 @@
+class Raindrops
+  def self.convert(number)
+    @number = number
+    result = String.new
+   if @number.modulo(3) == 0
+     result = result + "Pling"
+   end
+   if @number.modulo(5) == 0
+    result = result + "Plang"
+   end
+   if @number.modulo(7) == 0
+    result = result + "Plong"
+   end
+   result.empty? ? "#{number}" : result
+  end
+end
+
+
