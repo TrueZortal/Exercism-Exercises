e14a498ff131682e83c19ef3719591096176550e
Mateusz Buczek <mtsbck@gmail.com>
2021-12-24 11:34:29 +0100

    v 1

---
+++ ruby/scrabble-score/scrabble_score.rb
@@ -0,0 +1,19 @@
+class Scrabble
+  $scores ={"a" => 1,"e" => 1,"i" => 1,"o" => 1,"u" => 1,"l" => 1,"n" => 1,"r" => 1,"s" => 1,"t" => 1, "d" => 2, "g" => 2, "b" => 3,"c" => 3,"m" => 3,"p" => 3, "f" => 4,"h" => 4,"v" => 4,"w" => 4,"y" => 4, "k" => 5, "j" => 8,"x" => 8, "q" => 10, "z" => 10}
+  def initialize(word)
+    @word = word.to_s.downcase.strip.scan(/\w/)
+  end
+  def score
+    letterscore = []
+    index = 0
+    while index < @word.length do
+      letterscore << $scores[@word[index]]
+      index += 1
+    end
+    letterscore.sum
+  end
+  def self.score(word)
+    new(word).score
+  end
+end
+
