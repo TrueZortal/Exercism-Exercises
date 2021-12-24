40c609277e31d21e1c74454b98a4daf76ea737b4
Mateusz Buczek <mtsbck@gmail.com>
2021-12-24 22:33:42 +0100

    v 1, had to look up gsubbing out non break spaces

---
+++ ruby/luhn/luhn.rb
@@ -0,0 +1,31 @@
+class Luhn
+  def self.valid?(text)
+    @text = text.gsub(/[[:space:]]/, '').strip.chars.reverse
+      if @text.length > 1 && @text.any?(/[^0-9 ]/) == false
+        index = 0
+        verify_validity = []
+        verify_validity << @text[index].to_i
+        index += 1
+        if  @text[index].to_i * 2 > 9
+          verify_validity << @text[index].to_i * 2 - 9
+        else
+          verify_validity << @text[index].to_i * 2
+        end
+        index += 1
+        while index < @text.length do
+          verify_validity << @text[index].to_i
+            if  @text[index+1].to_i * 2 > 9
+            verify_validity << @text[index+1].to_i * 2 - 9
+            else
+            verify_validity << @text[index+1].to_i * 2
+            end
+          index += 2
+        end
+        verify_validity.map! {|x| x.to_i}.sum.modulo(10) == 0
+      else
+      false
+      end
+    end
+  end
+
+
