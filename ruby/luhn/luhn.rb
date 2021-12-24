114766062cc893c194366d526fb72b467efdb388
Mateusz Buczek <mtsbck@gmail.com>
2021-12-24 22:49:23 +0100

    v2 cleared up

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -4,22 +4,15 @@ class Luhn
       if @text.length > 1 && @text.any?(/[^0-9 ]/) == false
         index = 0
         verify_validity = []
-        verify_validity << @text[index].to_i
-        index += 1
-        if  @text[index].to_i * 2 > 9
-          verify_validity << @text[index].to_i * 2 - 9
-        else
-          verify_validity << @text[index].to_i * 2
-        end
-        index += 1
         while index < @text.length do
           verify_validity << @text[index].to_i
-            if  @text[index+1].to_i * 2 > 9
-            verify_validity << @text[index+1].to_i * 2 - 9
+          index += 1
+            if  @text[index].to_i * 2 > 9
+            verify_validity << @text[index].to_i * 2 - 9
             else
-            verify_validity << @text[index+1].to_i * 2
+            verify_validity << @text[index].to_i * 2
             end
-          index += 2
+          index += 1
         end
         verify_validity.map! {|x| x.to_i}.sum.modulo(10) == 0
       else
@@ -27,5 +20,4 @@ class Luhn
       end
     end
   end
-
 
