5f5b2328f1dd4b4dbd8c5e3941201cf6129899db
Mateusz Buczek <mtsbck@gmail.com>
2021-12-27 17:16:53 +0100

    v 1.1 Passing all tests, cleaned

--- ruby/tournament/tournament_test.rb
+++ ruby/tournament/tournament_test.rb
@@ -4,7 +4,7 @@ require_relative 'tournament'
 # Common test data version: 1.4.0 ee01fe0
 class TournamentTest < Minitest::Test
   def test_just_the_header_if_no_input
-    # skip
+
     input = <<~INPUT
 
     INPUT
@@ -31,7 +31,6 @@ class TournamentTest < Minitest::Test
   end
 
   def test_a_win_can_also_be_expressed_as_a_loss
-    skip
     input = <<~INPUT
       Blithering Badgers;Allegoric Alaskans;loss
     INPUT
@@ -46,7 +45,6 @@ class TournamentTest < Minitest::Test
   end
 
   def test_a_different_team_can_win
-    skip
     input = <<~INPUT
       Blithering Badgers;Allegoric Alaskans;win
     INPUT
@@ -61,7 +59,6 @@ class TournamentTest < Minitest::Test
   end
 
   def test_a_draw_is_one_point_each
-    skip
     input = <<~INPUT
       Allegoric Alaskans;Blithering Badgers;draw
     INPUT
@@ -76,7 +73,6 @@ class TournamentTest < Minitest::Test
   end
 
   def test_there_can_be_more_than_one_match
-    skip
     input = <<~INPUT
       Allegoric Alaskans;Blithering Badgers;win
       Allegoric Alaskans;Blithering Badgers;win
@@ -92,7 +88,6 @@ class TournamentTest < Minitest::Test
   end
 
   def test_there_can_be_more_than_one_winner
-    skip
     input = <<~INPUT
       Allegoric Alaskans;Blithering Badgers;loss
       Allegoric Alaskans;Blithering Badgers;win
@@ -108,7 +103,6 @@ class TournamentTest < Minitest::Test
   end
 
   def test_there_can_be_more_than_two_teams
-    skip
     input = <<~INPUT
       Allegoric Alaskans;Blithering Badgers;win
       Blithering Badgers;Courageous Californians;win
@@ -126,7 +120,6 @@ class TournamentTest < Minitest::Test
   end
 
   def test_typical_input
-    skip
     input = <<~INPUT
       Allegoric Alaskans;Blithering Badgers;win
       Devastating Donkeys;Courageous Californians;draw
@@ -148,7 +141,6 @@ class TournamentTest < Minitest::Test
   end
 
   def test_incomplete_competition_not_all_pairs_have_played
-    skip
     input = <<~INPUT
       Allegoric Alaskans;Blithering Badgers;loss
       Devastating Donkeys;Allegoric Alaskans;loss
@@ -168,7 +160,6 @@ class TournamentTest < Minitest::Test
   end
 
   def test_ties_broken_alphabetically
-    skip
     input = <<~INPUT
       Courageous Californians;Devastating Donkeys;win
       Allegoric Alaskans;Blithering Badgers;win
