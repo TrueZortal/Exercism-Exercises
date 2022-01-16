2358a02295ac40a2f95eb16ed0230d329c6e3514
Mateusz Buczek <mtsbck@gmail.com>
2022-01-16 19:04:02 +0100

    v2 after rubocop

--- ruby/resistor-color/resistor_color.rb
+++ ruby/resistor-color/resistor_color.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class ResistorColor
-  TABLE = {"black" => 0, "brown" => 1, "red" => 2, "orange" => 3, "yellow" => 4, "green" => 5, "blue" => 6, "violet" => 7, "grey" => 8, "white" => 9}
+  TABLE = { 'black' => 0, 'brown' => 1, 'red' => 2, 'orange' => 3, 'yellow' => 4, 'green' => 5, 'blue' => 6,
+            'violet' => 7, 'grey' => 8, 'white' => 9 }.freeze
   COLORS = TABLE.keys
   def self.color_code(given_color)
     TABLE[given_color]
