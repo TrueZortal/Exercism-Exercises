6585f73fd4d8e56bdd9ec5a98939cf4c969910b3
Mateusz Buczek <mtsbck@gmail.com>
2022-01-16 19:33:23 +0100

    v 2 after rubocop changed from for to each

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
 class Complement
   def self.of_dna(given_value)
     @rna = given_value.chars
     complement = String.new
-    for var in @rna
-      if var == 'G'
-        complement = complement + 'C'
-      elsif var == 'C'
-        complement = complement + 'G'
-      elsif var == 'T'
-        complement = complement + 'A'
-      elsif var == 'A'
-        complement = complement + 'U'
+    @rna.each do |var|
+      case var
+      when 'G'
+        complement += 'C'
+      when 'C'
+        complement += 'G'
+      when 'T'
+        complement += 'A'
+      when 'A'
+        complement += 'U'
       end
     end
     complement
   end
 end
-
