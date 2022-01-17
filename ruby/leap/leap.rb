b39998e0bf91792d7572ae93f0a2c1a0602f0c93
Mateusz Buczek <mtsbck@gmail.com>
2022-01-17 17:36:47 +0100

    v2 after rubocop

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class Year
   def self.leap?(given_value)
     @given_value = given_value.to_i
-    divisible_by_4 = @given_value % 4 == 0
+    divisible_by_4 = (@given_value % 4).zero?
     not_divisible_by_100 = @given_value % 100 != 0
-    divisible_by_400 = @given_value % 400 == 0
+    divisible_by_400 = (@given_value % 400).zero?
     if divisible_by_4
       if not_divisible_by_100
         true
@@ -19,5 +21,3 @@ class Year
     end
   end
 end
-
-
