a2b05bcde3fc1da2385708b0d53fe205977cf7b2
Mateusz Buczek <mtsbck@gmail.com>
2022-01-17 19:02:48 +0100

    v 666 nienawidze pipeow

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -3,8 +3,6 @@
 class Complement
   @complements_of_RNA_thingies = { 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }
   def self.of_dna(given_value)
-    complement = String.new
-    given_value.chars.map { |x| complement += @complements_of_RNA_thingies[x] }
-    complement
+    given_value.chars.reduce(String.new) { |complement, x| complement += @complements_of_RNA_thingies[x] }
   end
 end
