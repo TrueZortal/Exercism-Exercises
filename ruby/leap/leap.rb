50f06ce1a7f48fd15f2f02a934356175847f8d3b
Mateusz Buczek <mtsbck@gmail.com>
2022-01-17 17:52:07 +0100

    v4 after community using ternary

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -2,22 +2,6 @@
 
 class Year
   def self.leap?(given_value)
-    @given_value = given_value.to_i
-    divisible_by_4 = (@given_value % 4).zero?
-    not_divisible_by_100 = @given_value % 100 != 0
-    divisible_by_400 = (@given_value % 400).zero?
-    if divisible_by_4
-      if not_divisible_by_100
-        true
-      elsif not_divisible_by_100 && divisible_by_400
-        true
-      elsif not_divisible_by_100 == false && divisible_by_400
-        true
-      else
-        false
-      end
-    else
-      false
-    end
+    (given_value % 400).zero? || (given_value % 4).zero? && given_value % 100 != 0 ? true : false
   end
 end
