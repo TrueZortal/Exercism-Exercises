f9dc55440643279349cbe47828fa8f64b68c6679
Mateusz Buczek <mtsbck@gmail.com>
2022-01-17 17:53:50 +0100

    duplicate of V4

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -2,10 +2,6 @@
 
 class Year
   def self.leap?(given_value)
-    @given_value = given_value.to_i
-    divisible_by_4 = (@given_value % 4).zero?
-    not_divisible_by_100 = @given_value % 100 != 0
-    divisible_by_400 = (@given_value % 400).zero?
-    divisible_by_400 || divisible_by_4 && not_divisible_by_100 ? true : false
+    (given_value % 400).zero? || (given_value % 4).zero? && given_value % 100 != 0 ? true : false
   end
 end
