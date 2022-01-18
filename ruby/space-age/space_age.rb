02be3eb158bd0c2c027860aec69358725b65a4c6
Mateusz Buczek <mtsbck@gmail.com>
2022-01-18 22:41:06 +0100

    Attempted writing based on solution with no viewi

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,47 +1,26 @@
 # frozen_string_literal: true
 
 class SpaceAge
+
+EARTH_YEAR = 31_557_600
+MODIFIERS = {
+  earth: 1,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
+}
+
   def initialize(given_input)
-    earth_year_in_seconds = 31_557_600
-    @earth_years = given_input / earth_year_in_seconds.to_f
+    @seconds = given_input
   end
 
-  def on_mercury
-    modifier = 0.2408467
-    @earth_years / modifier
-  end
-
-  def on_venus
-    modifier = 0.61519726
-    @earth_years / modifier
-  end
-
-  def on_earth
-    @earth_years
-  end
-
-  def on_mars
-    modifier = 1.8808158
-    @earth_years / modifier
-  end
-
-  def on_jupiter
-    modifier = 11.862615
-    @earth_years / modifier
-  end
-
-  def on_saturn
-    modifier = 29.447498
-    @earth_years / modifier
-  end
-
-  def on_uranus
-    modifier = 84.016846
-    @earth_years / modifier
-  end
-
-  def on_neptune
-    modifier = 164.79132
-    @earth_years / modifier
+  MODIFIERS.each do |planet, orbit|
+    define_method ('on_'+ planet.to_s) do
+      @seconds.to_f / (EARTH_YEAR * orbit)
+    end
   end
 end
