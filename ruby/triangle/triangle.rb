7105c0de3a5269ad5a748014b447915477bf2082
Mateusz Buczek <mtsbck@gmail.com>
2022-01-18 22:28:56 +0100

    V2, rubocop -A , expected self redundancy

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -1,26 +1,28 @@
+# frozen_string_literal: true
+
 class Triangle
-  def initialize (given_input)
+  def initialize(given_input)
     @sides = given_input
   end
 
   def equilateral?
-    self.verify_triangularity_total ? self.unique_entries == 1 : false
+    verify_triangularity_total ? unique_entries == 1 : false
   end
 
   def isosceles?
-    if self.verify_triangularity_total
-      self.equilateral? ? true : self.unique_entries == 2
+    if verify_triangularity_total
+      equilateral? ? true : unique_entries == 2
     else
       false
     end
   end
 
   def scalene?
-    self.verify_triangularity_total ? self.unique_entries == 3 : false
+    verify_triangularity_total ? unique_entries == 3 : false
   end
 
   def degenerate?
-    self.verify_triangularity_total && self.verify_triangular_degeneracy ? true : false
+    verify_triangularity_total && verify_triangular_degeneracy ? true : false
   end
 
   private
@@ -30,28 +32,26 @@ class Triangle
   end
 
   def verify_triangularity_total
-    self.verify_triangularity_no_zero_sides && self.verify_triangularity_side_length
+    verify_triangularity_no_zero_sides && verify_triangularity_side_length
   end
 
   def verify_triangularity_no_zero_sides
-    @sides.select { |x| x == 0}.empty?
+    @sides.select(&:zero?).empty?
   end
 
   def verify_triangularity_side_length
     triangularity = String.new
     @sides.each do |x|
-      triangularity += (x.to_i <= (@sides.sum.to_i - x.to_i)).to_s + " "
+      triangularity += "#{x.to_i <= (@sides.sum.to_i - x.to_i)} "
     end
-    triangularity.split.all? {|sides| sides == 'true'}
+    triangularity.split.all? { |sides| sides == 'true' }
   end
 
   def verify_triangular_degeneracy
     triangularity = String.new
     @sides.each do |x|
-      triangularity += (x == @sides.sum - x).to_s + " "
+      triangularity += "#{x == @sides.sum - x} "
     end
-    triangularity.split.any? {|sides| sides == 'true'}
+    triangularity.split.any? { |sides| sides == 'true' }
   end
-
 end
-
