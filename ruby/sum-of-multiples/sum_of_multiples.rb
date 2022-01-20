d50e7d8ac13fb923e263afed1a6bc65ebbddacc8
Mateusz Buczek <mtsbck@gmail.com>
2022-01-20 21:04:06 +0100

    v2 Rubocopped

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -1,6 +1,7 @@
-class SumOfMultiples
+# frozen_string_literal: true
 
-  def initialize (*numbers)
+class SumOfMultiples
+  def initialize(*numbers)
     @numbers = numbers
     @array_of_unique_multiples = []
   end
@@ -16,14 +17,10 @@ class SumOfMultiples
   private
 
   def array_of_unique_multiples(number)
-    list_upto_upper_limit = (1..@upper_limit-1).to_a
+    list_upto_upper_limit = (1..@upper_limit - 1).to_a
     list_upto_upper_limit.each do |x|
-      if (x % number).zero? && !@array_of_unique_multiples.include?(x)
-        @array_of_unique_multiples << x
-      end
+      @array_of_unique_multiples << x if (x % number).zero? && !@array_of_unique_multiples.include?(x)
     end
     @array_of_unique_multiples
   end
-
 end
-
