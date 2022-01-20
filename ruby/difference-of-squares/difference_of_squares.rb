fd64eda6e4a4da6f58dcdb3e4cf9c39379b9412d
Mateusz Buczek <mtsbck@gmail.com>
2022-01-20 19:46:37 +0100

    V2 after rubocop improvements

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,11 +1,12 @@
-class Squares
+# frozen_string_literal: true
 
+class Squares
   def initialize(upper_limit)
     @array_of_natural_numbers_upto_upper_limit = (0..upper_limit).to_a
   end
 
   def sum_of_squares
-    @array_of_natural_numbers_upto_upper_limit.map { |x| x = x**2}.sum
+    @array_of_natural_numbers_upto_upper_limit.map { |x| x **= 2 }.sum
   end
 
   def square_of_sum
@@ -15,7 +16,4 @@ class Squares
   def difference
     square_of_sum.to_i - sum_of_squares.to_i
   end
-
 end
-
-
