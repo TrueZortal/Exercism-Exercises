5a1e2cea3b8ebac418341408e1ac7d304b46a1ee
Mateusz Buczek <mtsbck@gmail.com>
2022-01-20 20:20:46 +0100

    v2 rubocop + removed empty if statement

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Anagram
   def initialize(anagram)
     @anagram = anagram.downcase
@@ -7,14 +9,12 @@ class Anagram
   def match(list_to_test_against)
     result = []
     list_to_test_against.each do |x|
-       if x.downcase == @anagram
-       elsif x.downcase.chars.sort == @anagram_array
+      if x.downcase == @anagram
+        # do nothing
+      elsif x.downcase.chars.sort == @anagram_array
         result << x
-       end
+      end
     end
     result
   end
-
 end
-
-
