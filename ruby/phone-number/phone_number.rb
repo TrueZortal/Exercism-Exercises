5c806a638e0d89c3592bd8f7df88fc8ce859e745
Mateusz Buczek <mtsbck@gmail.com>
2022-02-20 19:02:45 +0100

    cleaned up v3

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -2,18 +2,18 @@
 
 class PhoneNumber
   def self.clean(phone_number)
-    @phone_number_array_clean_string = phone_number.split(/[^0-9]/).join
-    @struct_test = @phone_number_array_clean_string.chars.map(&:to_i)
-    if @phone_number_array_clean_string.length == 10 && @struct_test[0].between?(2,9) && @struct_test[3].between?(2, 9)
-      @phone_number_array_clean_string
-    elsif @phone_number_array_clean_string.length == 11
-      if @struct_test[0] != 1
-        # do nothing
-      elsif @struct_test[0] == 1 && @struct_test[1].between?(2, 9) && @struct_test[4].between?(2, 9)
-        @phone_number_array_clean_string[1, 10]
-      end
-    else
-      #do nothing
+    @struct_test = phone_number.split(/[^0-9]/).join.chars.map(&:to_i)
+    check_conditions ? @struct_test.last(10).join : nil
+  end
+
+  private
+
+  def self.check_conditions
+    case @struct_test.length
+    when 10
+      @struct_test.last(10)[0].between?(2, 9) && @struct_test.last(10)[3].between?(2, 9)
+    when 11
+      @struct_test.last(10)[0].between?(2, 9) && @struct_test.last(10)[3].between?(2, 9) && @struct_test[0] == 1
     end
   end
 end
