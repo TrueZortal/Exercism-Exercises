f75cc41037937c178f3d8e7eef47cdc4a974fe1c
Mateusz Buczek <mtsbck@gmail.com>
2022-02-20 19:42:39 +0100

    cleaner and with upto

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,19 +1,16 @@
+# frozen_string_literal: true
+
 class Grains
   def self.square(sqr_number)
-    if sqr_number.between?(1, 64)
-      sqr_number == 1 ? 1 : 2**(sqr_number-1)
-    else
-      raise ArgumentError
-    end
+    raise ArgumentError unless sqr_number.between?(1, 64)
+      sqr_number == 1 ? 1 : 2**(sqr_number - 1)
   end
 
   def self.total
-    squares_on_the_board = Array(1..64)
-    values = []
-    squares_on_the_board.each do |square|
-      square == 1 ? values << 1 : values << 2**(square-1)
+    values = [1]
+    2.upto(64) do |square|
+      values << 2**(square - 1)
     end
     values.sum
   end
 end
-
