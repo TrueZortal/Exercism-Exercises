9e1035de619e3b97dbb76a8bd51cac5c5b4caec1
Mateusz Buczek <mtsbck@gmail.com>
2022-02-21 09:52:32 +0100

    v2 rubocopped

--- ruby/collatz-conjecture/collatz_conjecture.rb
+++ ruby/collatz-conjecture/collatz_conjecture.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
+
 class CollatzConjecture
   def self.steps(number)
     raise ArgumentError unless number >= 1
+
     index = 0
-      until number == 1
-        if number%2 == 0
-          number = number/2
-          index += 1
-        else
-          number = 3*number + 1
-          index += 1
-        end
-      end
-      index
+    until number == 1
+      number = if number.even?
+                 number / 2
+               else
+                 3 * number + 1
+               end
+      index += 1
+    end
+    index
   end
 end
-
