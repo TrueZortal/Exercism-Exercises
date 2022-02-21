c7159ca05ff59e9842faa263f7ca4a33d7d0fc9e
Mateusz Buczek <mtsbck@gmail.com>
2022-02-21 12:50:10 +0100

    initial version pre linting

---
+++ ruby/sieve/sieve.rb
@@ -0,0 +1,27 @@
+require 'byebug'
+class Sieve
+  def initialize(limit)
+    @limit = limit
+    @full_array = Array(2..limit)
+  end
+
+  def primes
+    primes = []
+    not_primes = []
+    @full_array.each do |x|
+      if not_primes.include?(x)
+        #do nothing
+      else
+        primes << x
+        index = 2
+        until x * index > @limit
+          not_primes << x * index
+          index += 1
+        end
+      end
+    end
+    primes
+  end
+end
+
+
