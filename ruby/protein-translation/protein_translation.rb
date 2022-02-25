164a135b9dbfa9f8ca236462ee4866e6c721ad39
Mateusz Buczek <mtsbck@gmail.com>
2022-02-25 21:35:52 +0100

    V2 linted

--- ruby/protein-translation/protein_translation.rb
+++ ruby/protein-translation/protein_translation.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 class InvalidCodonError < StandardError
 end
 
 class Translation
   @dictionary = [
-    {name: 'Methionine',codon: 'AUG'},
-    {name: 'Phenylalanine', codon: 'UUU UUC'},
-    {name: 'Leucine', codon: 'UUA UUG'},
-    {name: 'Serine', codon: 'UCU UCC UCA UCG'},
-    {name: 'Tyrosine', codon: 'UAU UAC'},
-    {name: 'Cysteine', codon: 'UGU UGC'},
-    {name: 'Tryptophan', codon: 'UGG'},
-    {name: 'STOP', codon: 'UAA UAG UGA'}
+    { name: 'Methionine', codon: 'AUG' },
+    { name: 'Phenylalanine', codon: 'UUU UUC' },
+    { name: 'Leucine', codon: 'UUA UUG' },
+    { name: 'Serine', codon: 'UCU UCC UCA UCG' },
+    { name: 'Tyrosine', codon: 'UAU UAC' },
+    { name: 'Cysteine', codon: 'UGU UGC' },
+    { name: 'Tryptophan', codon: 'UGG' },
+    { name: 'STOP', codon: 'UAA UAG UGA' }
   ]
   def self.of_codon(codon)
     codon
-    @dictionary.select {|x| x[:codon].include?(codon)}[0][:name]
+    @dictionary.select { |x| x[:codon].include?(codon) }[0][:name]
   end
 
   def self.of_rna(strand)
@@ -22,15 +24,13 @@ class Translation
     array_of_codons << strand.slice!(0..2) until strand.empty?
     translation = []
     array_of_codons.each do |check|
-      begin
-      if @dictionary.select {|x| x[:codon].include?(check)}[0][:name] != 'STOP'
-       translation << @dictionary.select {|x| x[:codon].include?(check)}[0][:name]
+      if @dictionary.select { |x| x[:codon].include?(check) }[0][:name] != 'STOP'
+        translation << @dictionary.select { |x| x[:codon].include?(check) }[0][:name]
       else
         break
       end
-      rescue
-        raise InvalidCodonError
-      end
+    rescue StandardError
+      raise InvalidCodonError
     end
     translation
   end
