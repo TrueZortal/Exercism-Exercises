83fe3123feb2199b92bf418b4507d0750723ce08
Mateusz Buczek <mtsbck@gmail.com>
2022-02-26 14:35:50 +0100

    linted with small improvements v2

--- ruby/atbash-cipher/atbash_cipher.rb
+++ ruby/atbash-cipher/atbash_cipher.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 class Atbash
+  @alphabet = *('a'..'z')
+  @decryption_key = @alphabet.reverse.zip(@alphabet).to_h
+  @encryption_key = @alphabet.zip(@alphabet.reverse).to_h
 
   def self.encode(message)
     @message = message.downcase.chars
@@ -8,40 +13,34 @@ class Atbash
 
   def self.decode(code)
     @code = code.chars
-    if @code.any? {|x| x.to_i != 0}
+    if @code.any? { |x| x.to_i != 0 }
       secret_message = []
       @code.each do |x|
-        if x.to_i != 0
-          secret_message << x
-        else
-          secret_message << @decryption_key[x]
-        end
+        secret_message << if x.to_i != 0
+                            x
+                          else
+                            @decryption_key[x]
+                          end
       end
       secret_message = secret_message.join
     else
-      @code.map {|x| x = @decryption_key[x]}.join
+      @code.map { |x| @decryption_key[x] }.join
     end
   end
 
-  private
-
-  @alphabet = *('a'..'z')
-  @decryption_key = @alphabet.reverse.zip(@alphabet).to_h
-  @encryption_key = @alphabet.zip(@alphabet.reverse).to_h
-
   def self.encrypt
-    if @message.any? {|x| x.to_i != 0}
+    if @message.any? { |x| x.to_i != 0 }
       @encrypted_message = []
       @message.each do |x|
-        if x.to_i != 0
-          @encrypted_message << x
-        else
-          @encrypted_message << @encryption_key[x]
-        end
+        @encrypted_message << if x.to_i != 0
+                                x
+                              else
+                                @encryption_key[x]
+                              end
       end
       @encrypted_message = @encrypted_message.join
     else
-      @encrypted_message = @message.map {|x| x = @encryption_key[x]}.join
+      @encrypted_message = @message.map { |x| @encryption_key[x] }.join
     end
   end
 
@@ -50,14 +49,9 @@ class Atbash
       @encrypted_message
     else
       longer_encrypted_message = String.new
-      while @encrypted_message.length > 5
-        longer_encrypted_message << "#{@encrypted_message.slice!(0..4)} "
-      end
+      longer_encrypted_message << "#{@encrypted_message.slice!(0..4)} " while @encrypted_message.length > 5
       longer_encrypted_message << @encrypted_message
       longer_encrypted_message
     end
   end
 end
-
-
-
