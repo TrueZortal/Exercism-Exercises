42f06e491c6d8a0b2dee8fd1193bc81bede732dc
Mateusz Buczek <mtsbck@gmail.com>
2022-02-27 18:10:11 +0100

    linted v2

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 class Integer
   def to_roman
-
     alphabet = {
       1 => 'I',
       5 => 'V',
@@ -11,33 +12,31 @@ class Integer
       1000 => 'M'
     }
 
-  number = self
-  digits_of_number = number.digits
-  converted_number = []
-  exponent = 1
-  digits_of_number.each do |x|
-    if x <= 3
-    1.upto(x) do
-      converted_number <<  exponent.to_i
-    end
-    elsif x == 4
-      converted_number << exponent.to_i * 5
-      converted_number <<  exponent.to_i
-    elsif x == 5
-      converted_number << (exponent*5)
-    elsif x <= 8
-      1.upto(x-5) do
-        converted_number <<  exponent
+    number = self
+    digits_of_number = number.digits
+    converted_number = []
+    exponent = 1
+    digits_of_number.each do |x|
+      if x <= 3
+        1.upto(x) do
+          converted_number << exponent.to_i
+        end
+      elsif x == 4
+        converted_number << exponent.to_i * 5
+        converted_number <<  exponent.to_i
+      elsif x == 5
+        converted_number << (exponent * 5)
+      elsif x <= 8
+        1.upto(x - 5) do
+          converted_number << exponent
+        end
+        converted_number << (exponent * 5).to_i
+      elsif x == 9
+        converted_number << (exponent.to_i * 10)
+        converted_number <<  exponent.to_i
       end
-      converted_number << (exponent*5).to_i
-    elsif x == 9
-      converted_number << (exponent.to_i * 10)
-      converted_number <<  exponent.to_i
+      exponent *= 10
     end
-    exponent *= 10
+    converted_number.map { |x| alphabet[x] }.reverse.join
   end
-  converted_number.map {|x| alphabet[x]}.reverse.join
-  end
-
 end
-
