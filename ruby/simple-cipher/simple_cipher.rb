bdcc693d13c6260fa8fdbdddab5576a2aca629cb
Mateusz Buczek <mtsbck@gmail.com>
2022-02-27 11:38:00 +0100

    v2 linted

--- ruby/simple-cipher/simple_cipher.rb
+++ ruby/simple-cipher/simple_cipher.rb
@@ -1,33 +1,35 @@
-class Cipher
+# frozen_string_literal: true
 
+class Cipher
   numbers = *(0..25)
   alphabet = *('a'..'z')
-  $alphabet_numbers_match = numbers.zip(alphabet).to_h
+  @@alphabet_numbers_match = numbers.zip(alphabet).to_h
 
   attr_reader :key
 
-  def initialize(input = self.generate_key)
-  raise ArgumentError unless (input.split(/[a-z]/).empty? && input != '') if input != nil
+  def initialize(input = generate_key)
+    raise ArgumentError if !input.nil? && !(input.split(/[a-z]/).empty? && input != '')
 
-  @key = input
+    @key = input
   end
 
   def generate_key
     automated_key = String.new
     0.upto(100) do
-      automated_key << $alphabet_numbers_match[rand(26)]
+      automated_key << @@alphabet_numbers_match[rand(26)]
     end
     automated_key
   end
 
   def encode(plaintext)
-    plaintext.chars.each_with_index.map {|x, i| x = $alphabet_numbers_match[($alphabet_numbers_match.key(x)+$alphabet_numbers_match.key(@key[i % @key.length])) % 26]}.join
+    plaintext.chars.each_with_index.map do |x, i|
+      @@alphabet_numbers_match[(@@alphabet_numbers_match.key(x) + @@alphabet_numbers_match.key(@key[i % @key.length])) % 26]
+    end.join
   end
 
   def decode(ciphertext)
-    ciphertext.chars.each_with_index.map {|x, i| x = $alphabet_numbers_match[($alphabet_numbers_match.key(x)-$alphabet_numbers_match.key(@key[i % @key.length])) % 26]}.join
+    ciphertext.chars.each_with_index.map do |x, i|
+      @@alphabet_numbers_match[(@@alphabet_numbers_match.key(x) - @@alphabet_numbers_match.key(@key[i % @key.length])) % 26]
+    end.join
   end
 end
-
-
-
