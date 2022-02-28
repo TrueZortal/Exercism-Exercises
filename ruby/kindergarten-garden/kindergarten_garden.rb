53361447008644a044b308722819b6a0c7ed38a6
Mateusz Buczek <mtsbck@gmail.com>
2022-02-28 18:07:36 +0100

    v2, linted, got lazy with tabs in v1 -.-

--- ruby/kindergarten-garden/kindergarten_garden.rb
+++ ruby/kindergarten-garden/kindergarten_garden.rb
@@ -1,5 +1,6 @@
-class Garden
+# frozen_string_literal: true
 
+class Garden
   @@plants = {
     'G' => :grass,
     'C' => :clover,
@@ -7,22 +8,21 @@ class Garden
     'V' => :violets
   }
 
-  def initialize(garden_text, students = ['alice', 'bob', 'charlie', 'david', 'eve', 'fred', 'ginny', 'harriet', 'ileana', 'joseph', 'kincaid', 'larry'])
-    @students = students.sort.map {|x| x.downcase}
+  def initialize(garden_text, students = %w[alice bob charlie david eve fred ginny harriet
+                                            ileana joseph kincaid larry])
+    @students = students.sort.map(&:downcase)
     @garden = garden_text.lines
     create_individual_student_methods
   end
 
   def create_individual_student_methods
     @students.each do |student|
-      define_singleton_method (student) do
+      define_singleton_method(student) do
         index = @students.index(student) * 2
-        garden = [@@plants[@garden[0][index]], @@plants[@garden[0][index+1]], @@plants[@garden[1][index]], @@plants[@garden[1][index+1]]]
+        garden = [@@plants[@garden[0][index]], @@plants[@garden[0][index + 1]], @@plants[@garden[1][index]],
+                  @@plants[@garden[1][index + 1]]]
         garden
       end
     end
   end
-
 end
-
-
