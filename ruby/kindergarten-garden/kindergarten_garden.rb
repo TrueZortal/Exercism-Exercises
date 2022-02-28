2d5ee99f0b34073dae4d0ce41141d6c13d3e9f92
Mateusz Buczek <mtsbck@gmail.com>
2022-02-28 18:06:36 +0100

    v1 unlinted

---
+++ ruby/kindergarten-garden/kindergarten_garden.rb
@@ -0,0 +1,28 @@
+class Garden
+
+  @@plants = {
+    'G' => :grass,
+    'C' => :clover,
+    'R' => :radishes,
+    'V' => :violets
+  }
+
+  def initialize(garden_text, students = ['alice', 'bob', 'charlie', 'david', 'eve', 'fred', 'ginny', 'harriet', 'ileana', 'joseph', 'kincaid', 'larry'])
+    @students = students.sort.map {|x| x.downcase}
+    @garden = garden_text.lines
+    create_individual_student_methods
+  end
+
+  def create_individual_student_methods
+    @students.each do |student|
+      define_singleton_method (student) do
+        index = @students.index(student) * 2
+        garden = [@@plants[@garden[0][index]], @@plants[@garden[0][index+1]], @@plants[@garden[1][index]], @@plants[@garden[1][index+1]]]
+        garden
+      end
+    end
+  end
+
+end
+
+
