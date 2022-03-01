e189f5b2f0cefef548a144eeb6946dc4454cfa69
Mateusz Buczek <mtsbck@gmail.com>
2022-03-01 22:58:45 +0100

    V2 linted

--- ruby/isbn-verifier/isbn_verifier.rb
+++ ruby/isbn-verifier/isbn_verifier.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class IsbnVerifier
   def self.valid?(isbn)
     @isbn = isbn.split(/[^0-9X]/).join.chars.reverse
@@ -6,13 +8,13 @@ class IsbnVerifier
     if @isbn.include?('X') && @isbn.index('X') != 0 || @isbn.length != 10
       return false
     else
-      @isbn = @isbn.map {|x| x == 'X' ? '10' : x}
+      @isbn = @isbn.map { |x| x == 'X' ? '10' : x }
       @isbn.each do |x|
-        verification_array << x.to_i*i
+        verification_array << x.to_i * i
         i += 1
       end
     end
+
     (verification_array.sum % 11).zero?
   end
 end
-
