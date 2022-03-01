c03020ae9122beaa6298b61f38cc5bb03d5fb24f
Mateusz Buczek <mtsbck@gmail.com>
2022-03-01 19:57:44 +0100

    v2 linted

--- ruby/largest-series-product/largest_series_product.rb
+++ ruby/largest-series-product/largest_series_product.rb
@@ -1,40 +1,42 @@
+# frozen_string_literal: true
+
 class Series
   def initialize(numbers)
     raise ArgumentError unless numbers.upcase == numbers && numbers.downcase == numbers
 
-    @numbers = numbers.split(/[^\d]/).join.chars.map {|x| x.to_i}
+    @numbers = numbers.split(/[^\d]/).join.chars.map(&:to_i)
   end
 
   def largest_product(of_how_many)
-    raise ArgumentError unless of_how_many.to_i >= 0  && of_how_many.to_i <= @numbers.length
+    raise ArgumentError unless of_how_many.to_i >= 0 && of_how_many.to_i <= @numbers.length
 
     array_of_products = []
     @index = 0
-    if of_how_many == 0
+    if of_how_many.zero?
       array_of_products = [1]
     else
-      @numbers.each do |x|
-      if @numbers[@index+of_how_many-1] == nil
-        break
-      else
-        array_of_products << compute_the_product(of_how_many)
-      end
-      @index += 1
+      @numbers.each do |_x|
+        if @numbers[@index + of_how_many - 1].nil?
+          break
+        else
+          array_of_products << compute_the_product(of_how_many)
+        end
+
+        @index += 1
       end
     end
     array_of_products.max
   end
 
-private
+  private
 
-def compute_the_product(limit)
-  temp_array = []
-  temp_order_tracker = @index
-  temp_order_tracker.upto(temp_order_tracker+limit-1) do
-    temp_array << @numbers[temp_order_tracker]
-    temp_order_tracker += 1
+  def compute_the_product(limit)
+    temp_array = []
+    temp_order_tracker = @index
+    temp_order_tracker.upto(temp_order_tracker + limit - 1) do
+      temp_array << @numbers[temp_order_tracker]
+      temp_order_tracker += 1
+    end
+    temp_array.reduce(:*)
   end
-  temp_array.reduce(:*)
-end
-
 end
