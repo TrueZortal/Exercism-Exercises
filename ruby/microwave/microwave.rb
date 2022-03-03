099e50a9a778cf7d245fc4edde3ed986ac29f33a
Mateusz Buczek <mtsbck@gmail.com>
2022-03-03 17:02:24 +0100

    linted v2

--- ruby/microwave/microwave.rb
+++ ruby/microwave/microwave.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class Microwave
   def initialize(time)
     @time = time.digits.reverse
     @base_timer = [0, 0, 0, 0]
     @base_timer = @base_timer.drop(@time.join.length)
-    @base_timer =  @base_timer.append(@time).flatten
+    @base_timer = @base_timer.append(@time).flatten
     if @base_timer[2] > 5
       @base_timer[2] = (@base_timer.flatten[2] % 6)
       @base_timer[1] += 1
@@ -11,7 +13,6 @@ class Microwave
   end
 
   def timer
-    @base_timer.each_slice(2).map {|x| x.join}.join(':')
+    @base_timer.each_slice(2).map(&:join).join(':')
   end
 end
-
