61d629a5ed87198fa1e1c589933e0250fabd5be5
Mateusz Buczek <mtsbck@gmail.com>
2022-03-07 17:15:44 +0100

    v1

---
+++ ruby/darts/darts.rb
@@ -0,0 +1,20 @@
+class Darts
+
+  def initialize(x,y)
+    @x = x
+    @y = y
+    @r = Math.sqrt(@x**2 + @y**2)
+  end
+
+  def score
+    if @r <= 1
+      10
+    elsif @r > 1 && @r <= 5
+      5
+    elsif @r > 5 && @r <= 10
+      1
+    else
+      0
+    end
+  end
+end
