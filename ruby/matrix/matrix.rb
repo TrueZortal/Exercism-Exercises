6a123466d29641cfa7c3f85ccc6805eeec80ef18
Mateusz Buczek <mtsbck@gmail.com>
2022-03-08 12:14:29 +0100

    v1 transposing without transpose

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,11 +1,22 @@
 class Matrix
-
-  attr_reader :rows
-  attr_reader :columns
+attr_reader :rows
 
   def initialize(given_input)
-  @rows = given_input.lines.map { |i| i.split.map(&:to_i)}
-  @columns = @rows.transpose
+  @rows = given_input.split(/\n/).map { |x| x.strip.split(' ').map(&:to_i) }
+  end
+
+  def columns
+    index = 0
+    all_columns = []
+    1.upto(@rows.max.length) do
+      individual_column = []
+      @rows.each do |row|
+        individual_column << row[index]
+      end
+      all_columns << individual_column
+      index += 1
+    end
+    all_columns
   end
 end
 
