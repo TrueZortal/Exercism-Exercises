f685d036bbb260799bf7026b5063669ee5835732
Mateusz Buczek <mtsbck@gmail.com>
2022-03-08 17:17:50 +0100

    v2.5 linted

--- ruby/pythagorean-triplet/pythagorean_triplet.rb
+++ ruby/pythagorean-triplet/pythagorean_triplet.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 class Triplet
   attr_reader :sum, :product
 
   def initialize(a, b, c)
-    @a, @b, @c = a, b, c
+    @a = a
+    @b = b
+    @c = c
     @array_of_sides = [@a, @b, @c]
     @sum = @array_of_sides.sum
     @product = @array_of_sides.reduce(&:*)
@@ -12,19 +16,18 @@ class Triplet
     @a**2 + @b**2 == @c**2
   end
 
-  def self.where(sum: nil, min_factor: 1,max_factor:)
+  def self.where(max_factor:, sum: nil, min_factor: 1)
     array_of_all_required_values = *(min_factor..max_factor)
     working_array = array_of_all_required_values.combination(3).to_a
-    if sum != nil
-        working_array.keep_if do |x|
-          x[0]**2 + x[1]**2 == x[2]**2 && x.sum == sum
-        end
-      else
-        working_array.keep_if do |x|
-          x[0]**2 + x[1]**2 == x[2]**2
-        end
+    if !sum.nil?
+      working_array.keep_if do |x|
+        x[0]**2 + x[1]**2 == x[2]**2 && x.sum == sum
       end
-      working_array.map! {|x| x = new(x[0],x[1],x[2])}
+    else
+      working_array.keep_if do |x|
+        x[0]**2 + x[1]**2 == x[2]**2
+      end
+    end
+    working_array.map! { |x| new(x[0], x[1], x[2]) }
   end
 end
-
