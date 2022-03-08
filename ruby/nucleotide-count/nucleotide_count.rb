afce6e9429ff055060b7b315a24d346321cafede
Mateusz Buczek <mtsbck@gmail.com>
2022-03-08 13:21:04 +0100

    v 1.5 linted

--- ruby/nucleotide-count/nucleotide_count.rb
+++ ruby/nucleotide-count/nucleotide_count.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 class Nucleotide
+  VALID_NUCLEOTIDES = %w[A T G C].freeze
 
-  VALID_NUCLEOTIDES = ['A', 'T', 'G', 'C']
-
-  def initialize (dna_slice)
-    raise ArgumentError unless dna_slice.chars.all? {|x| VALID_NUCLEOTIDES.include?(x)} || dna_slice == ''
+  def initialize(dna_slice)
+    raise ArgumentError unless dna_slice.chars.all? { |x| VALID_NUCLEOTIDES.include?(x) } || dna_slice == ''
 
     @dna_slice = dna_slice
   end
@@ -20,9 +21,7 @@ class Nucleotide
     histogram_of_nucleotides = {}
     VALID_NUCLEOTIDES.each do |nucleotide|
       histogram_of_nucleotides[nucleotide] = 0
-      if @dna_slice != ''
-      histogram_of_nucleotides[nucleotide] = count(nucleotide)
-      end
+      histogram_of_nucleotides[nucleotide] = count(nucleotide) if @dna_slice != ''
     end
     histogram_of_nucleotides
   end
