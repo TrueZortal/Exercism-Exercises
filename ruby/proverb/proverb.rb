1a406ddc612d9e8049128a25d584401c365e68fa
Mateusz Buczek <mtsbck@gmail.com>
2022-03-08 17:56:12 +0100

    v 1.5 linted

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -1,15 +1,16 @@
-class Proverb
+# frozen_string_literal: true
 
+class Proverb
   def initialize(*items, qualifier: nil)
     @items = items
     index = 0
     @proverb = []
 
-    until index+1 == @items.length
-    first_item = @items[index]
-    second_item = @items[index+1]
-    @proverb << "For want of a #{first_item} the #{second_item} was lost.\n"
-    index += 1
+    until index + 1 == @items.length
+      first_item = @items[index]
+      second_item = @items[index + 1]
+      @proverb << "For want of a #{first_item} the #{second_item} was lost.\n"
+      index += 1
     end
     @proverb << "And all for the want of a #{qualifier.nil? ? '' : "#{qualifier} "}#{@items[0]}."
   end
@@ -17,7 +18,4 @@ class Proverb
   def to_s
     @proverb.join
   end
-
 end
-
-
