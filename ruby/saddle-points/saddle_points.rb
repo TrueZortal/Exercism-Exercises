49c27745a16b1f6908f8a9ab88e563d499db02ed
Mateusz Buczek <mtsbck@gmail.com>
2022-03-08 11:46:18 +0100

    v1, hell yeah

---
+++ ruby/saddle-points/saddle_points.rb
@@ -0,0 +1,21 @@
+class Matrix
+
+  attr_reader :rows, :columns
+
+  def initialize(matrix)
+    @rows = matrix.lines.map {|x| x.strip.split(' ').map(&:to_i)}
+    @columns = @rows.transpose
+  end
+
+  def saddle_points
+    array_of_saddle_points = []
+    @rows.each_with_index do |row, row_index|
+      row.each_with_index do |number, column|
+        if number == @columns[column].min { |a, b| a <=> b } && number == row.max { |a, b| a <=> b }
+          array_of_saddle_points << [row_index, column]
+        end
+      end
+    end
+    array_of_saddle_points
+  end
+end
