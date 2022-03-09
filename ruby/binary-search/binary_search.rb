9a1ac45095c6c6dff5eab1527032d542afd5c038
Mateusz Buczek <mtsbck@gmail.com>
2022-03-09 12:39:30 +0100

    v1.5 linted

--- ruby/binary-search/binary_search.rb
+++ ruby/binary-search/binary_search.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class BinarySearch
   def initialize(values)
     @values = values.sort
@@ -5,33 +7,31 @@ class BinarySearch
 
   def search_for(lost)
     @lost = lost
-    @index = *(0..@values.size-1)
+    @index = *(0..@values.size - 1)
     @values_with_index = @values.zip(@index).to_h
     if @values == []
-      return nil
+      nil
+    elsif @lost == @values_with_index.key(halfway_point_value)
+      halfway_point_value
     else
-      if @lost == @values_with_index.key(halfway_point_value)
-        halfway_point_value
-      else
-        until @lost == @values_with_index.key(halfway_point_value)
-          if @values_with_index.size != 1 && @lost < @values_with_index.key(halfway_point_value)
-            @values_with_index = @values_with_index.each_slice(half_of_the_hash).to_a[0].to_h
-          elsif @values_with_index.size != 1 && @lost > @values_with_index.key(halfway_point_value)
-            @values_with_index = @values_with_index.each_slice(half_of_the_hash).to_a[1].to_h
-          else
-            return nil
-          end
+      until @lost == @values_with_index.key(halfway_point_value)
+        if @values_with_index.size != 1 && @lost < @values_with_index.key(halfway_point_value)
+          @values_with_index = @values_with_index.each_slice(half_of_the_hash).to_a[0].to_h
+        elsif @values_with_index.size != 1 && @lost > @values_with_index.key(halfway_point_value)
+          @values_with_index = @values_with_index.each_slice(half_of_the_hash).to_a[1].to_h
+        else
+          return nil
         end
-        @values_with_index.values.join.to_i
       end
+      @values_with_index.values.join.to_i
     end
   end
 
   def halfway_point_value
-    @values_with_index.to_a[@values_with_index.to_a.size/2.0.round][1]
+    @values_with_index.to_a[@values_with_index.to_a.size / 2.0.round][1]
   end
 
   def half_of_the_hash
-    (@values_with_index.size/2.0).round
+    (@values_with_index.size / 2.0).round
   end
 end
