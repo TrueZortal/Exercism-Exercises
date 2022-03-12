6b694aa9b2d3df85cb231e07c314f22cb570c997
TrueZortal <mtsbck@gmail.comm>
2022-03-12 18:16:06 +0100

    v1.5 linted

--- ruby/allergies/allergies.rb
+++ ruby/allergies/allergies.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
 class Allergies
   attr_reader :list
- @@allergens = {
-   1 =>'eggs',
-   2 => 'peanuts',
-   4 => 'shellfish',
-   8 => 'strawberries', 
-   16 => 'tomatoes',
-   32 => 'chocolate',
-   64 => 'pollen',
-   128 => 'cats'
- }
+
+  @@allergens = {
+    1 => 'eggs',
+    2 => 'peanuts',
+    4 => 'shellfish',
+    8 => 'strawberries',
+    16 => 'tomatoes',
+    32 => 'chocolate',
+    64 => 'pollen',
+    128 => 'cats'
+  }
   def initialize(allergy_score)
     @allergy_score = allergy_score % 256
     list_of_allergies
@@ -23,12 +26,10 @@ class Allergies
   def list_of_allergies
     @list_of_allergies = []
     @@allergens.keys.reverse.each do |x|
-     if x <= @allergy_score
-      @list_of_allergies << @@allergens[x]
-      @allergy_score -= x
-     end
+      if x <= @allergy_score
+        @list_of_allergies << @@allergens[x]
+        @allergy_score -= x
+      end
     end
   end
-
 end
-
