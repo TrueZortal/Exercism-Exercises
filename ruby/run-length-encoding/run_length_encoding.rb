895d0008b54ebd98e7479ae99931e6f9ba694415
Mateusz Buczek <mtsbck@gmail.com>
2022-03-16 00:14:58 +0100

    v1

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -3,20 +3,47 @@ class RunLengthEncoding
         @string = string.chars
         @answer = []
         @char_set = []
-        p @string
-        @string.each do |char|
+        @string.each_with_index do |char, index|
             if !@char_set.empty? && !@char_set.include?(char)
                 if @char_set.size == 1
                     @answer << @char_set.join
                     @char_set = []
+                    @char_set << char
                 else
                     @answer << "#{@char_set.size}#{@char_set[0]}"
                     @char_set = []
+                    @char_set << char
                 end
             else
-                @char_set << char
+                if index+1 == @string.size
+                    @char_set << char
+                    @answer << "#{@char_set.size}#{@char_set[0]}"
+                    @char_set = []
+                else
+                    @char_set << char
+                end
             end
         end
-        p @answer.join
+        if !@char_set.empty?
+            @answer << @char_set
+        end
+        @answer.join
+    end
+
+    def self.decode(code)
+        @code = code.chars
+        @deciphered = []
+        @code.each_with_index do |char, index|
+            if @code[index-1].to_i != 0
+                next
+            elsif char.to_i != 0 && @code[index+1].to_i == 0
+                @deciphered << @code[index+1] * char.to_i
+            elsif char.to_i != 0 && @code[index+1].to_i != 0
+                @deciphered << @code[index+2] * (char.to_i * 10 + @code[index+1].to_i * 1)
+            else
+                @deciphered << char
+            end
+        end
+        @deciphered.join
     end
 end
