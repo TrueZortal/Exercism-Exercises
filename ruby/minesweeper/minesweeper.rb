b2956de39fedb09e7f8d9fd5652e252b91103617
Mateusz Buczek <mtsbck@gmail.com>
2022-03-16 17:14:45 +0100

    v1,5 linted

--- ruby/minesweeper/minesweeper.rb
+++ ruby/minesweeper/minesweeper.rb
@@ -1,25 +1,30 @@
+# frozen_string_literal: true
+
 class Board
-  ALLOWED_CHARS = ['|', '-', '+', '*', ' ']
+  ALLOWED_CHARS = ['|', '-', '+', '*', ' '].freeze
 
   def self.transform(input)
-    raise ArgumentError unless input.min.length == input.max.length && input.join.chars.all? {|x| ALLOWED_CHARS.include?(x)} && input.first == input.last
-    @array_of_rows = input.map {|x| x.chars}
+    raise ArgumentError unless input.min.length == input.max.length && input.join.chars.all? do |x|
+                                 ALLOWED_CHARS.include?(x)
+                               end && input.first == input.last
+
+    @array_of_rows = input.map(&:chars)
     @frame = ['|', '-', '+']
     @da_bomb = '*'
     @empty = ' '
     @array_of_rows.each_with_index do |row, row_index|
       row.each_with_index do |elem, elem_index|
-        if elem == @da_bomb
-          temp_array_of_neightobring_rows = [row_index -1, row_index, row_index +1]
-          temp_array_of_neighbors = [elem_index -1, elem_index, elem_index +1]
-          temp_array_of_neightobring_rows.each do |neighboring_row|
-            temp_array_of_neighbors.each do |neighboring_elem|
-              if @array_of_rows[neighboring_row][neighboring_elem] != @da_bomb && !@frame.include?(@array_of_rows[neighboring_row][neighboring_elem])
-                if @array_of_rows[neighboring_row][neighboring_elem] == ' '
-                  @array_of_rows[neighboring_row][neighboring_elem] = 1
-                else
+        next unless elem == @da_bomb
+
+        temp_array_of_neightobring_rows = [row_index - 1, row_index, row_index + 1]
+        temp_array_of_neighbors = [elem_index - 1, elem_index, elem_index + 1]
+        temp_array_of_neightobring_rows.each do |neighboring_row|
+          temp_array_of_neighbors.each do |neighboring_elem|
+            if @array_of_rows[neighboring_row][neighboring_elem] != @da_bomb && !@frame.include?(@array_of_rows[neighboring_row][neighboring_elem])
+              if @array_of_rows[neighboring_row][neighboring_elem] == ' '
+                @array_of_rows[neighboring_row][neighboring_elem] = 1
+              else
                 @array_of_rows[neighboring_row][neighboring_elem] += 1
-                end
               end
             end
           end
@@ -29,4 +34,3 @@ class Board
     @array_of_rows.map!(&:join)
   end
 end
-
