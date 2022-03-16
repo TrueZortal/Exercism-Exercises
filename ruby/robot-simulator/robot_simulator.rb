25c1c7cf2d92e35bb955aa870e5951f9fa21ad47
Mateusz Buczek <mtsbck@gmail.com>
2022-03-16 23:41:10 +0100

    v1

--- ruby/robot-simulator/robot_simulator.rb
+++ ruby/robot-simulator/robot_simulator.rb
@@ -4,10 +4,13 @@ class Robot
   DIRECTIONS = {west: -1, north:+1, east: +1, south: -1}
 
   def initialize
+    @bearing = nil
+    @coordinates = nil
   end
 
   def orient(direction)
     raise ArgumentError unless DIRECTIONS.keys.include?(direction)
+
     @bearing = direction
   end
 
@@ -41,6 +44,30 @@ class Robot
 end
 
 class Simulator
-  def initialize
+  COMMANDS = {
+    'L' => :turn_left,
+    'R' => :turn_right,
+    'A' => :advance
+  }
+
+  def instructions(list_of_commands)
+    @commands = list_of_commands.chars
+    @array_of_commands = []
+    @commands.each do |command|
+      @array_of_commands << COMMANDS[command]
+    end
+    @array_of_commands
+  end
+
+  def place(robot, coordinates_and_bearing)
+    robot.orient(coordinates_and_bearing[:direction])
+    robot.at(coordinates_and_bearing[:x],coordinates_and_bearing[:y])
+  end
+
+  def evaluate(robot, commands)
+    instructions(commands).each do |command|
+      robot.send(command)
+    end
   end
 end
+
--- ruby/robot-simulator/robot_simulator_test.rb
+++ ruby/robot-simulator/robot_simulator_test.rb
@@ -129,28 +129,28 @@ class RobotSimulatorTest < Minitest::Test
   end
 
   def test_instructions_for_turning_left
-    skip
+    # skip
     assert_equal [:turn_left], simulator.instructions('L')
   end
 
   def test_instructions_for_turning_right
-    skip
+    # skip
     assert_equal [:turn_right], simulator.instructions('R')
   end
 
   def test_instructions_for_advancing
-    skip
+    # skip
     assert_equal [:advance], simulator.instructions('A')
   end
 
   def test_series_of_instructions
-    skip
+    # skip
     commands = %i[turn_right advance advance turn_left]
     assert_equal commands, simulator.instructions('RAAL')
   end
 
   def test_instruct_robot
-    skip
+    # skip
     robot = Robot.new
     simulator.place(robot, x: -2, y: 1, direction: :east)
     simulator.evaluate(robot, 'RLAALAL')
@@ -159,7 +159,7 @@ class RobotSimulatorTest < Minitest::Test
   end
 
   def test_instruct_many_robots
-    skip
+    # skip
     robot1 = Robot.new
     robot2 = Robot.new
     robot3 = Robot.new
