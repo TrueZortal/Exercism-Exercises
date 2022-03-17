a5755a9289304a3217daab1b4c77b028828fc9d2
Mateusz Buczek <mtsbck@gmail.com>
2022-03-18 00:15:16 +0100

    v1

--- ruby/secret-handshake/secret_handshake.rb
+++ ruby/secret-handshake/secret_handshake.rb
@@ -7,12 +7,25 @@ COMMANDS = {
 1000 => 'jump',
 10000 => :reverse
 }
+
   def initialize(decimal)
-    @decimal = decimal.to_s(2)
-    p @decimal
+    if decimal.is_a? Integer
+    @decimal = decimal.to_s(2).chars
+    else
+    @decimal = [nil]
+    end
   end
 
   def commands
-    [@decimal]
+    commands = []
+    @decimal.reverse.each_with_index do |command, index|
+      action = COMMANDS[command.to_i * (10 ** index)]
+      if action != nil && index != 4
+        commands << COMMANDS[command.to_i * (10 ** index)]
+      elsif action == :reverse
+        commands = commands.reverse
+      end
+    end
+    commands
   end
 end
