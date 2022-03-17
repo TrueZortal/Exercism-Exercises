ae7c0d9f8a5f55bf42d013b82d5f53382717811d
Mateusz Buczek <mtsbck@gmail.com>
2022-03-18 00:15:43 +0100

    v1.3 linted

--- ruby/secret-handshake/secret_handshake.rb
+++ ruby/secret-handshake/secret_handshake.rb
@@ -1,27 +1,28 @@
-class SecretHandshake
+# frozen_string_literal: true
 
-COMMANDS = {
-1 => 'wink',
-10 => 'double blink',
-100 => 'close your eyes',
-1000 => 'jump',
-10000 => :reverse
-}
+class SecretHandshake
+  COMMANDS = {
+    1 => 'wink',
+    10 => 'double blink',
+    100 => 'close your eyes',
+    1000 => 'jump',
+    10_000 => :reverse
+  }.freeze
 
   def initialize(decimal)
-    if decimal.is_a? Integer
-    @decimal = decimal.to_s(2).chars
-    else
-    @decimal = [nil]
-    end
+    @decimal = if decimal.is_a? Integer
+                 decimal.to_s(2).chars
+               else
+                 [nil]
+               end
   end
 
   def commands
     commands = []
     @decimal.reverse.each_with_index do |command, index|
-      action = COMMANDS[command.to_i * (10 ** index)]
-      if action != nil && index != 4
-        commands << COMMANDS[command.to_i * (10 ** index)]
+      action = COMMANDS[command.to_i * (10**index)]
+      if !action.nil? && index != 4
+        commands << COMMANDS[command.to_i * (10**index)]
       elsif action == :reverse
         commands = commands.reverse
       end
