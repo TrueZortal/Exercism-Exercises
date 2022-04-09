5bca327630afd5cb57515bdcb21c35688d5e771b
TrueZortal <mtsbck@gmail.comm>
2022-04-09 22:31:31 +0200

    v1.5 linted

--- ruby/simple-linked-list/simple_linked_list.rb
+++ ruby/simple-linked-list/simple_linked_list.rb
@@ -1,59 +1,61 @@
+# frozen_string_literal: true
+
 class Element
-    attr_accessor :datum, :next, :previous
-    def initialize(data)
-        @datum = data
-        @next = nil
-        @previous = nil
-    end
+  attr_accessor :datum, :next, :previous
+
+  def initialize(data)
+    @datum = data
+    @next = nil
+    @previous = nil
+  end
 end
 
-class SimpleLinkedList 
-    def initialize(array = [])
-        @array = []
-        if array != []
-            array.each do |elem|
-                @array << Element.new(elem)
-            end
-        end
+class SimpleLinkedList
+  def initialize(array = [])
+    @array = []
+    if array != []
+      array.each do |elem|
+        @array << Element.new(elem)
+      end
     end
+  end
 
-    def push(elem)
-        if @array.length == 0 
-            @array[@array.length] = elem 
-        else
-            @array[@array.length-1].next = elem
-            @array[@array.length] = elem
-            elem.previous = @array
-        end
-        self
+  def push(elem)
+    if @array.length.zero?
+      @array[@array.length] = elem
+    else
+      @array[@array.length - 1].next = elem
+      @array[@array.length] = elem
+      elem.previous = @array
     end
+    self
+  end
 
-    def pop(x = 0)
-        return nil if x >= @array.size
+  def pop(x = 0)
+    return nil if x >= @array.size
 
-        if x == 0 
-            temp = @array[@array.size-1]
-            @array.delete_at(@array.size-1)
-            temp
-        else
-            @array[(@array.size-1-x)..(@array.size-1)]
-        end
+    if x.zero?
+      temp = @array[@array.size - 1]
+      @array.delete_at(@array.size - 1)
+      temp
+    else
+      @array[(@array.size - 1 - x)..(@array.size - 1)]
     end
+  end
 
-    def to_a
-        @array.reverse.map { |x| x.datum }
+  def to_a
+    @array.reverse.map(&:datum)
+  end
+
+  def reverse!
+    array = []
+    @array.each do |x|
+      temp = x.next
+      x.next = x.previous
+      x.next = temp
+      array.unshift(x)
     end
-
-    def reverse!
-        array = []
-        @array.each do |x|
-            temp = x.next
-            x.next = x.previous
-            x.next = temp
-            array.unshift(x)
-        end
-        @array = array
-        self
-    end
-
+    @array = array
+    self
+  end
 end
