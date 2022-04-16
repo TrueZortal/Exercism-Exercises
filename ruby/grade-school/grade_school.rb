910cffafe8b88f2fb482961a9bb9ea43b4f17855
Mateusz Buczek <mtsbck@gmail.com>
2022-04-16 10:54:50 +0200

    v1.1 linted

--- ruby/grade-school/grade_school.rb
+++ ruby/grade-school/grade_school.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class School
-  def initialize()
+  def initialize
     @roster = []
   end
 
   def students(grade)
-    return [] if @roster.none? {|x| x.grade == grade}
+    return [] if @roster.none? { |x| x.grade == grade }
 
     find_students_within_a_grade(grade)
   end
@@ -22,28 +24,30 @@ class School
   private
 
   def find_students_within_a_grade(grade)
-    @roster.select {|student| student.grade == grade}.map { |student_within_a_grade| student_within_a_grade.name }.sort
+    @roster.select do |student|
+      student.grade == grade
+    end.map(&:name).sort
   end
 
   def establish_present_grades
-    @grades = @roster.uniq {|student| student.grade}.map {|student_to_grade| student_to_grade.grade}
+    @grades = @roster.uniq(&:grade).map(&:grade)
   end
 
   def build_the_roster
     establish_present_grades
     full_roster = []
     @grades.each do |grade|
-      full_roster << {grade: grade, students: find_students_within_a_grade(grade)}
+      full_roster << { grade: grade, students: find_students_within_a_grade(grade) }
     end
-    full_roster.sort_by! {|grade_roster| grade_roster[:grade]}
+    full_roster.sort_by! { |grade_roster| grade_roster[:grade] }
   end
 
   class Student
     attr_reader :name, :grade
+
     def initialize(name, grade)
       @name = name
       @grade = grade
     end
   end
-
 end
