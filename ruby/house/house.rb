9666d5a1070d94c68d65516bee64d4fc42b462d0
Mateusz Buczek <mtsbck@gmail.com>
2022-04-18 11:19:18 +0200

    v1 string ops are fun

---
+++ ruby/house/house.rb
@@ -0,0 +1,33 @@
+class House
+  def self.recite
+    lines = [
+      ['lay in', 'the house that Jack built.'],
+      ['ate', 'the malt'],
+      ['killed', 'the rat'],
+      ['worried', 'the cat'],
+      ['tossed', 'the dog'],
+      ['milked', 'the cow with the crumpled horn'],
+      ['kissed', 'the maiden all forlorn'],
+      ['married', 'the man all tattered and torn'],
+      ['woke', 'the priest all shaven and shorn'],
+      ['kept', 'the rooster that crowed in the morn'],
+      ['belonged to', 'the farmer sowing his corn'],
+      ['', 'the horse and the hound and the horn']
+    ]
+
+    rhyme = String.new
+
+    0.upto(lines.size-1) do |line_number|
+      rhyme << "This is #{lines[line_number][1]}\n"
+      the_rest =  (0..line_number-1).to_a.reverse
+      the_rest.each do |remaining_line|
+         rhyme << "that #{lines[remaining_line][0]} #{lines[remaining_line][1]}\n"
+      end
+      if line_number != lines.size-1
+        rhyme << "\n"
+      end
+    end
+    rhyme
+  end
+end
+
