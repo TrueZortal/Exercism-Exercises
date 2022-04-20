edd137237816f017f597a02e98c11e50159bfc6c
Mateusz Buczek <mtsbck@gmail.com>
2022-04-20 22:02:25 +0200

    v1 initial setup

--- ruby/poker/poker.rb
+++ ruby/poker/poker.rb
@@ -1,5 +1,34 @@
-class ClassName
-  def initialize
+class Poker
+  @@Figures = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  def initialize(hands)
 
   end
+
+  def same_suit?(hand)
+
+  end
+
+  def sequential?(hand)
+
+  end
+
+  def how_many_of_same?(hand)
+
+  end
+
 end
+
+# Straight flush = sequential 5 in same colour x
+# Four of a kind x
+# Full House 2+3 figures
+# Flush 5 of same suit x
+# Straight 5 sequential x
+# Three of a kind x
+# Two pair 2+2 x
+# One pair 2 x
+# High card
+
+is sequential?
+is same suit?
+how_many_of_same?
+
