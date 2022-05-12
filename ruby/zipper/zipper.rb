9b9d5c110e0a3110059525eb3a956a69349436ce
Mateusz Buczek <mtsbck@gmail.com>
2022-05-12 20:43:19 +0200

    v0.1 dis gonn b gud, recursion 101

---
+++ ruby/zipper/zipper.rb
@@ -0,0 +1,75 @@
+class Zipper
+  def self.from_tree(tree)
+  end
+
+  def self.list(tree)
+    # PRE-ORDER
+    p tree.value
+    if tree.left != nil
+      list(tree.left)
+    end
+
+    if tree.right != nil
+      list(tree.right)
+    end
+
+    # POST-ORDER
+    # if tree.left != nil
+    #   list(tree.left)
+    # end
+    # if tree.right != nil
+    #   list(tree.right)
+    # end
+    # p tree
+
+  end
+
+  def self.to_tree
+  end
+end
+
+class Node
+  attr_accessor :value, :left, :right
+  def initialize(value = nil, left = nil, right = nil)
+    @value = value
+    @left = left
+    @right = right
+  end
+
+
+end
+
+# Node.new(1,
+
+# (1,(2,nil,(3,nil,nil)),(4,nil,nil))
+# value ,L/R, Parent, Sibling
+
+# (nil,1,nil,nil,(L,2,1,(4,nil,nil)(R,3,2,nil)),(R,4,1,(2,nil,3)))
+
+# L/R
+# value = value
+# Parent
+# Sibling of left = right
+# Sibling of right = left
+
+
+
+
+tree =
+      Node.new(1,
+        Node.new(2,
+          nil,
+          Node.new(3,
+            nil,
+            nil)),
+        Node.new(4,
+          nil,
+          nil))
+
+Zipper.list(tree)
+
+# p tree.to_s
+
+# random_thing = [nil,1,nil,nil,['L',2,1,[4,nil,nil],['R',3,2,nil]],['R',4,1,[2,nil,3]]]
+# p random_thing.size
+# # array_of_arrays = [[]]
