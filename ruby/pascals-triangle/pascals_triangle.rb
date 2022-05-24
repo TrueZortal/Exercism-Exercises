d95291d64238fa8afa77a1a217d1b4f723d6642f
Mateusz Buczek <mtsbck@gmail.com>
2022-05-24 18:27:13 +0200

    v1

--- ruby/pascals-triangle/pascals_triangle.rb
+++ ruby/pascals-triangle/pascals_triangle.rb
@@ -1,7 +1,40 @@
-=begin
-Write your code for the 'Pascals Triangle' exercise in this file. Make the tests in
-`pascals_triangle_test.rb` pass.
+class Triangle
+  def initialize(num_of_rows)
+    @num_of_rows = num_of_rows
+    build
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/pascals-triangle` directory.
-=end
+  def rows
+    @triangulated_rows
+  end
+
+  private
+
+  def build
+    @triangulated_rows = [[1]]
+    i = 2
+
+    2.upto(@num_of_rows) do |index|
+      @row = []
+      if index > 2
+        fill(@triangulated_rows[index-2])
+      end
+      add_leading_trailing_ones
+      @triangulated_rows << @row
+    end
+  end
+
+  def fill(row)
+    row.each_with_index do |number, inner_index|
+      if inner_index <= row.size - 2
+        @row << number + row[inner_index + 1]
+      end
+    end
+  end
+
+  def add_leading_trailing_ones
+    one = 1
+    @row << one
+    @row.unshift(one)
+  end
+end
