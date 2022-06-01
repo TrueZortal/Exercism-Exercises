2989eabb596fc6bc219f6975ff84fbbdc34eff2d
TrueZortal <mtsbck@gmail.comm>
2022-06-01 22:24:42 +0200

    hello world in js

---
+++ javascript/hello-world/hello-world.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+// export function hello() {
+//   return 'Goodbye, Mars!';
+// }
+
+export function hello() {
+  return 'Hello, World!';
+}
