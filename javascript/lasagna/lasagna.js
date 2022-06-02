61b5ad5ddf625c48ee85b98ac1c241f24b2c880b
TrueZortal <mtsbck@gmail.comm>
2022-06-02 08:11:43 +0200

    v1.1 lasagna linted

--- javascript/lasagna/lasagna.js
+++ javascript/lasagna/lasagna.js
@@ -54,7 +54,7 @@ export function remainingMinutesInOven(actualMinutesInOven) {
  * @returns {number} the total preparation time
  */
 export function preparationTimeInMinutes(numberOfLayers) {
-  return numberOfLayers * 2;
+  return numberOfLayers * PREPARATION_MINUTES_PER_LAYER;
 }
 
 /**
