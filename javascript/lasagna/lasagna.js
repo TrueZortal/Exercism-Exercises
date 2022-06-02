ab46882ad2e10918aa0bf85ccb9ef94e9da70a19
TrueZortal <mtsbck@gmail.comm>
2022-06-02 08:10:22 +0200

    v1 lasagna

--- javascript/lasagna/lasagna.js
+++ javascript/lasagna/lasagna.js
@@ -33,6 +33,7 @@
 /**
  * The number of minutes it takes to prepare a single layer.
  */
+export const EXPECTED_MINUTES_IN_OVEN = 40;
 const PREPARATION_MINUTES_PER_LAYER = 2;
 
 /**
@@ -43,7 +44,7 @@ const PREPARATION_MINUTES_PER_LAYER = 2;
  * @returns {number} the number of minutes remaining
  */
 export function remainingMinutesInOven(actualMinutesInOven) {
-  throw new Error('Remove this line and implement the function');
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
 }
 
 /**
@@ -53,7 +54,7 @@ export function remainingMinutesInOven(actualMinutesInOven) {
  * @returns {number} the total preparation time
  */
 export function preparationTimeInMinutes(numberOfLayers) {
-  throw new Error('Remove this line and implement the function');
+  return numberOfLayers * 2;
 }
 
 /**
@@ -65,5 +66,5 @@ export function preparationTimeInMinutes(numberOfLayers) {
  * @returns {number} the total working time
  */
 export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
-  throw new Error('Remove this line and implement the function');
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
 }
