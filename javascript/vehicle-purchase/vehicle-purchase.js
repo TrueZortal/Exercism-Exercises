6e0e5a3a275a35233e9a617892812866139af50c
TrueZortal <mtsbck@gmail.comm>
2022-06-04 15:27:26 +0200

    v1

---
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -0,0 +1,51 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Determines whether or not you need a licence to operate a certain kind of vehicle.
+ *
+ * @param {string} kind
+ * @returns {boolean} whether a license is required
+ */
+export function needsLicense(kind) {
+  if (kind === 'car' || kind === 'truck') {
+    return true;
+    } else {
+      return false;
+    }
+}
+
+/**
+ * Helps choosing between two options by recommending the one that
+ * comes first in dictionary order.
+ *
+ * @param {string} option1
+ * @param {string} option2
+ * @returns {string} a sentence of advice which option to choose
+ */
+export function chooseVehicle(option1, option2) {
+  let list_of_cars = [option1, option2];
+  list_of_cars.sort();
+  return list_of_cars[0] + ' is clearly the better choice.';
+}
+
+/**
+ * Calculates an estimate for the price of a used vehicle in the dealership
+ * based on the original price and the age of the vehicle.
+ *
+ * @param {number} originalPrice
+ * @param {number} age
+ * @returns expected resell price in the dealership
+ */
+export function calculateResellPrice(originalPrice, age) {
+  if (age < 3) {
+    return originalPrice * 0.8;
+  } else if (age >= 3 && age <= 10) {
+    return originalPrice * 0.7;
+  } else {
+    return originalPrice * 0.5;
+  }
+}
