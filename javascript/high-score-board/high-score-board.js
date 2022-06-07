2cdbc467b7818e5ddde3777f489540195e7f7871
Mateusz Buczek <mtsbck@gmail.com>
2022-06-07 23:15:37 +0200

    partial

---
+++ javascript/high-score-board/high-score-board.js
@@ -0,0 +1,72 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Creates a new score board with an initial entry.
+ *
+ * @returns {Record<string, number>} new score board
+ */
+export function createScoreBoard() {
+  const scoreboard = {
+    "The Best Ever": 1000000
+  }
+  return scoreboard;
+}
+
+/**
+ * Adds a player to a score board.
+ *
+ * @param {Record<string, number>} scoreBoard
+ * @param {string} player
+ * @param {number} score
+ * @returns {Record<string, number>} updated score board
+ */
+export function addPlayer(scoreBoard, player, score) {
+  scoreBoard[player] = score;
+  return scoreBoard;
+}
+
+/**
+ * Removes a player from a score board.
+ *
+ * @param {Record<string, number>} scoreBoard
+ * @param {string} player
+ * @returns {Record<string, number>} updated score board
+ */
+export function removePlayer(scoreBoard, player) {
+  delete scoreBoard[player];
+  return scoreBoard;
+}
+
+/**
+ * Increases a player's score by the given amount.
+ *
+ * @param {Record<string, number>} scoreBoard
+ * @param {string} player
+ * @param {number} points
+ * @returns {Record<string, number>} updated score board
+ */
+export function updateScore(scoreBoard, player, points) {
+  scoreBoard[player] = score;
+  return scoreBoard;
+}
+
+/**
+ * Applies 100 bonus points to all players on the board.
+ *
+ * @param {Record<string, number>} scoreBoard
+ * @returns {Record<string, number>} updated score board
+ */
+export function applyMondayBonus(scoreBoard) {
+  throw new Error('Please implement the applyMondayBonus function');
+}
+
+/**
+ * Normalizes a score with the provided normalization function.
+ *
+ * @param {Params} params the parameters for performing the normalization
+ * @returns {number} normalized score
+ */
+export function normalizeScore(params) {
+  throw new Error('Please implement the normalizeScore function');
+}
