74f2fcd5361cf3ea21973f8d01cd13e99a06b601
TrueZortal <mtsbck@gmail.comm>
2023-01-26 21:45:03 +0100

    C# 2/5 for #12in23: Committing annalyns infiltration  solution

---
+++ csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -0,0 +1,24 @@
+using System;
+
+static class QuestLogic
+{
+    public static bool CanFastAttack(bool knightIsAwake)
+    {
+        return !knightIsAwake;
+    }
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+    {
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    }
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+    {
+        return !archerIsAwake && prisonerIsAwake;
+    }
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+    {
+        return (petDogIsPresent && knightIsAwake && !archerIsAwake) || ( !knightIsAwake && !archerIsAwake && ((prisonerIsAwake && (!petDogIsPresent || petDogIsPresent) || (!prisonerIsAwake && petDogIsPresent))));
+    }
+}
