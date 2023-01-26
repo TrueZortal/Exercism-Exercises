8923d946d54ce27a69b38e8023ad33afc833b0ad
TrueZortal <mtsbck@gmail.comm>
2023-01-26 12:55:58 +0100

    BASH: Acronym, iteration 2

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -5,18 +5,8 @@ acronymator () {
   cleansed=${1//[^[:alnum:].\']/ }
   for word in $cleansed
   do
-    if [[ "$word" != "$word" ]]
-      then
-        for cleanword in $word
-        do
-          acronym+=${cleanword:0:1}
-        done
-      else
-        acronym+=${word:0:1}
-    fi
-    done
-
-
+    acronym+=${word:0:1}
+  done
   echo ${acronym^^}
 }
 
