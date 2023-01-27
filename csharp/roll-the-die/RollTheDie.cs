33c1f59ee98c09ffc26eff091e83c2e5997ef0aa
TrueZortal <mtsbck@gmail.comm>
2023-01-27 18:15:35 +0100

    C# Improvement to roll the die exercise - removing unneccessary System. before instantiating Random()

--- csharp/roll-the-die/RollTheDie.cs
+++ csharp/roll-the-die/RollTheDie.cs
@@ -4,13 +4,13 @@ public class Player
 {
     public int RollDie()
     {
-        var die = new System.Random(18);
+        var die = new Random(18);
         return die.Next(18);
     }
 
     public double GenerateSpellStrength()
     {
-        var die = new System.Random(100);
+        var die = new Random(100);
         return die.NextDouble();
     }
 }
