8c83667b9476349f02b9c7254ce886ad35235346
TrueZortal <mtsbck@gmail.comm>
2023-02-04 14:13:51 +0100

    Elixir 2/5 for #12in23: Removed reduntant variable assignment for anonymous functions, iteration 3 secrests

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,29 +1,29 @@
 defmodule Secrets do
   def secret_add(secret) do
-    adder = &(secret + &1)
+    &(secret + &1)
   end
 
   def secret_subtract(secret) do
-    substractor = &(&1 - secret)
+    &(&1 - secret)
   end
 
   def secret_multiply(secret) do
-    multiplier = &(&1 * secret)
+    &(&1 * secret)
   end
 
   def secret_divide(secret) do
-    divider = &(div(&1,secret))
+    &(div(&1,secret))
   end
 
   def secret_and(secret) do
-    ander = &(Bitwise.band(&1, secret))
+    &(Bitwise.band(&1, secret))
   end
 
   def secret_xor(secret) do
-    xorer = &(Bitwise.bxor(&1, secret))
+    &(Bitwise.bxor(&1, secret))
   end
 
   def secret_combine(secret_function1, secret_function2) do
-    combined = &(secret_function2.(secret_function1.(&1)))
+    &(secret_function2.(secret_function1.(&1)))
   end
 end
