5fdd39cae180421c84181d1cb7aa9ccf839da62a
TrueZortal <mtsbck@gmail.comm>
2023-02-04 14:10:46 +0100

    Elixir 2/5 for #12in23: Alternative solution for secrets with the shorthand anonymous function syntax

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,43 +1,29 @@
 defmodule Secrets do
   def secret_add(secret) do
-    adder = fn num ->
-      secret + num
-    end
+    adder = &(secret + &1)
   end
 
   def secret_subtract(secret) do
-    substractor = fn num ->
-      num - secret
-    end
+    substractor = &(&1 - secret)
   end
 
   def secret_multiply(secret) do
-    multiplier = fn num ->
-      num * secret
-    end
+    multiplier = &(&1 * secret)
   end
 
   def secret_divide(secret) do
-    divider = fn num ->
-      div(num,secret)
-    end
+    divider = &(div(&1,secret))
   end
 
   def secret_and(secret) do
-    ander = fn num ->
-      Bitwise.band(num, secret)
-    end
+    ander = &(Bitwise.band(&1, secret))
   end
 
   def secret_xor(secret) do
-    xorer = fn num ->
-      Bitwise.bxor(num, secret)
-    end
+    xorer = &(Bitwise.bxor(&1, secret))
   end
 
   def secret_combine(secret_function1, secret_function2) do
-    combined = fn num ->
-      secret_function2.(secret_function1.(num))
-    end
+    combined = &(secret_function2.(secret_function1.(&1)))
   end
 end
