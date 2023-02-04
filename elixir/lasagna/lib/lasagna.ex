f1ff5d851fbfb8c50f0822408014eaaf2d142fa6
TrueZortal <mtsbck@gmail.comm>
2023-02-04 13:30:26 +0100

    Elixir 1/5 for #12in23: Second iteration for lasagna with the shorthand syntax

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -1,21 +1,13 @@
 defmodule Lasagna do
-  def expected_minutes_in_oven do
-    40
-  end
+  def expected_minutes_in_oven, do: 40
 
-  def remaining_minutes_in_oven(time_in_oven) do
-    expected_minutes_in_oven()-time_in_oven
-  end
+  def remaining_minutes_in_oven(time_in_oven), do: expected_minutes_in_oven()-time_in_oven
 
-  def preparation_time_in_minutes(layers) do
-    layers * 2
-  end
+  def preparation_time_in_minutes(layers), do: layers * 2
 
   def total_time_in_minutes(layers, time_in_oven) do
     preparation_time_in_minutes(layers) + time_in_oven
   end
 
-  def alarm do
-    "Ding!"
-  end
+  def alarm, do: "Ding!"
 end
