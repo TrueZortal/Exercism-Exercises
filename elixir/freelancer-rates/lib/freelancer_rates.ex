329d96716f7297ad588794f29a2a8a7dc53bae52
TrueZortal <mtsbck@gmail.comm>
2023-02-05 22:35:04 +0100

    Elixir 3/5 for #12in23:comitting solution for freelancer-rates

---
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -0,0 +1,19 @@
+defmodule FreelancerRates do
+  def daily_rate(hourly_rate), do: hourly_rate * 8.0
+
+  def apply_discount(before_discount, discount), do: before_discount * ((100 - discount)/100)
+
+  def monthly_rate(hourly_rate, discount) do
+    trunc(
+      Float.ceil(
+          (8 * 22 * hourly_rate) * ((100 - discount)/100)
+        )
+      )
+  end
+
+  def days_in_budget(budget, hourly_rate, discount) do
+    Float.floor(
+        budget / apply_discount(daily_rate(hourly_rate),discount),1
+      )
+  end
+end
