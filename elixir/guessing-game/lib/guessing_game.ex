ee7186809ba16b391ae3ce2253c0d326c61fee78
TrueZortal <mtsbck@gmail.comm>
2023-02-16 19:40:08 +0100

    Elixir 7/5 for #12in23: improvements for guessing game solution, added default values defined in header, disabled unused var in clause

--- elixir/guessing-game/lib/guessing_game.ex
+++ elixir/guessing-game/lib/guessing_game.ex
@@ -1,5 +1,6 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess \\ :no_guess) when guess == :no_guess, do: "Make a guess"
+  def compare(secret_number, guess \\ :no_guess)
+  def compare(_secret_number, guess) when guess == :no_guess, do: "Make a guess"
   def compare(secret_number, guess) when secret_number == guess, do: "Correct"
   def compare(secret_number, guess) when secret_number in [guess+1, guess-1], do: "So close"
   def compare(secret_number, guess) when secret_number < guess, do: "Too high"
