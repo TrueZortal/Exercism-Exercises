eb61f923ead7a88686254dfb700e795692cda46b
TrueZortal <mtsbck@gmail.comm>
2023-02-17 19:01:48 +0100

    Elixir 8/5 for #12in23: removed unneccessary variable assignment kitchen calculator

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -7,11 +7,11 @@ defmodule KitchenCalculator do
   def to_milliliter({:teaspoon,_} = volume_pair), do: {:milliliter, (5 * get_volume(volume_pair))}
   def to_milliliter({:tablespoon,_} = volume_pair), do: {:milliliter, (15 * get_volume(volume_pair))}
 
-  def from_milliliter(volume_pair, measure = :milliliter), do: volume_pair
-  def from_milliliter(volume_pair, measure = :cup), do: {:cup, (get_volume(volume_pair)/240)}
-  def from_milliliter(volume_pair, measure = :fluid_ounce), do: {:fluid_ounce, (get_volume(volume_pair)/30)}
-  def from_milliliter(volume_pair, measure = :teaspoon), do: {:teaspoon, (get_volume(volume_pair)/5)}
-  def from_milliliter(volume_pair, measure = :tablespoon), do: {:tablespoon, (get_volume(volume_pair)/15)}
+  def from_milliliter(volume_pair, _ = :milliliter), do: volume_pair
+  def from_milliliter(volume_pair, _ = :cup), do: {:cup, (get_volume(volume_pair)/240)}
+  def from_milliliter(volume_pair, _ = :fluid_ounce), do: {:fluid_ounce, (get_volume(volume_pair)/30)}
+  def from_milliliter(volume_pair, _ = :teaspoon), do: {:teaspoon, (get_volume(volume_pair)/5)}
+  def from_milliliter(volume_pair, _ = :tablespoon), do: {:tablespoon, (get_volume(volume_pair)/15)}
 
   def convert(volume_pair, unit) do
     from_milliliter(to_milliliter(volume_pair), unit)
