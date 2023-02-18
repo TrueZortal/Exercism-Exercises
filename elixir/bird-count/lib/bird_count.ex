407e06a794685f07c051e9da4ed408244e118b37
TrueZortal <mtsbck@gmail.comm>
2023-02-18 13:12:16 +0100

    Elixir 9/5 for #12in23: partial solution for bird-count, recursion be recursive

---
+++ elixir/bird-count/lib/bird_count.ex
@@ -0,0 +1,19 @@
+defmodule BirdCount do
+  def today([] = list), do: nil
+  def today(list), do: hd(list)
+
+  def increment_day_count([] = list), do: [1]
+  def increment_day_count(list), do: [ hd(list)+1 | tl(list)]
+
+  def has_day_without_birds?([] = _list), do: false
+  def has_day_without_birds?([0 | _] = _list), do: true
+  def has_day_without_birds?(list), do: has_day_without_birds?(tl(list))
+
+  def total([] = list), do: 0
+  def total([:head | _] = list) when :head > 0, do: hd(list)
+  def total(list), do: total( [ (hd(list) + hd(tl(list))) | tl(tl(list)) ])
+
+  def busy_days(list) do
+
+  end
+end
