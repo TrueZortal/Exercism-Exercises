ffef413f48dc3f8a18e7c160e63c9576d8dd20d3
TrueZortal <mtsbck@gmail.comm>
2023-02-20 11:34:12 +0100

    Elixir 9/5 for #12in23: working solution for bird-count

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,24 +1,19 @@
 defmodule BirdCount do
-  def today([] = list), do: nil
+  def today([] = _list), do: nil
   def today(list), do: hd(list)
 
-  def increment_day_count([] = list), do: [1]
+  def increment_day_count([] = _list), do: [1]
   def increment_day_count(list), do: [ hd(list)+1 | tl(list)]
 
   def has_day_without_birds?([] = _list), do: false
   def has_day_without_birds?([0 | _] = _list), do: true
   def has_day_without_birds?(list), do: has_day_without_birds?(tl(list))
 
-  def total([] = list), do: 0
+  def total([] = _list), do: 0
   def total(list) when length(list) == 1, do: hd(list)
   def total(list), do: total( [ (hd(list) + hd(tl(list))) | tl(tl(list)) ])
 
   def busy_days([] = _list), do: 0
-  def busy_days(list) when length(list) == 1, do: hd(list)
-  def busy_days(list) do
-    cond do
-      hd(tl(list)) > 4 -> busy_days([hd(list)+1 | tl(tl(list))])
-      hd(tl(list)) <= 4 -> busy_days([hd(list) | tl(tl(list))])
-    end
-  end
+  def busy_days([current | tbd]) when current >= 5, do: 1 + busy_days(tbd)
+  def busy_days([_current | tbd]), do: busy_days(tbd)
 end
