885afdd13583cb703a34e271147cc4235ae0219c
TrueZortal <mtsbck@gmail.comm>
2023-02-20 11:41:47 +0100

    Elixir 9/5 for #12in23: improved solution for bird-count

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -6,12 +6,12 @@ defmodule BirdCount do
   def increment_day_count(list), do: [ hd(list)+1 | tl(list)]
 
   def has_day_without_birds?([] = _list), do: false
-  def has_day_without_birds?([0 | _] = _list), do: true
+  def has_day_without_birds?([0 | _]), do: true
   def has_day_without_birds?(list), do: has_day_without_birds?(tl(list))
 
   def total([] = _list), do: 0
-  def total(list) when length(list) == 1, do: hd(list)
-  def total(list), do: total( [ (hd(list) + hd(tl(list))) | tl(tl(list)) ])
+  def total(list) when length(list) == 1 , do: hd(list)
+  def total([count | remaining]), do: total( [ count + hd(remaining) | tl(remaining) ])
 
   def busy_days([] = _list), do: 0
   def busy_days([current | tbd]) when current >= 5, do: 1 + busy_days(tbd)
