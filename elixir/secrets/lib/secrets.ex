d7555e6400bc3f8a5bfed3bb142ab6b9e00e0525
TrueZortal <mtsbck@gmail.comm>
2023-03-04 10:00:00 +0100

    12in23 Elixir: Improved solution for secrests

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,29 +1,17 @@
 defmodule Secrets do
-  def secret_add(secret) do
-    &(secret + &1)
-  end
+  def secret_add(secret), do: &(secret + &1)
 
-  def secret_subtract(secret) do
-    &(&1 - secret)
-  end
+  def secret_subtract(secret), do: &(&1 - secret)
 
-  def secret_multiply(secret) do
-    &(&1 * secret)
-  end
+  def secret_multiply(secret), do: &(&1 * secret)
 
-  def secret_divide(secret) do
-    &(div(&1,secret))
-  end
+  def secret_divide(secret), do: &(div(&1,secret))
 
-  def secret_and(secret) do
-    &(Bitwise.band(&1, secret))
-  end
+  def secret_and(secret), do: &(Bitwise.band(&1, secret))
 
-  def secret_xor(secret) do
-    &(Bitwise.bxor(&1, secret))
-  end
+  def secret_xor(secret), do: &(Bitwise.bxor(&1, secret))
 
   def secret_combine(secret_function1, secret_function2) do
-    &(secret_function2.(secret_function1.(&1)))
+    &((secret_function1.(&1)) |> secret_function2.())
   end
 end
