9db8ee17c09db95ec852444dabc09c6bf462d4dd
TrueZortal <mtsbck@gmail.com>
2023-05-24 21:00:34 +0200

    Prolog 2/5 for #12in23L: Cooler solution to queen attack

--- prolog/queen-attack/queen_attack.pl
+++ prolog/queen-attack/queen_attack.pl
@@ -1,18 +1,11 @@
 create((DimX, DimY)) :-
 	maplist(between(0,7), [DimX, DimY]).
 
-attack((FromX, FromY), (ToX, ToY)):-
-	FromX =:= ToX,
+attack((X, _), (X, _)):-
+	!.
+
+attack((_,Y), (_, Y)):-
 	!.
 
 attack((FromX, FromY), (ToX, ToY)):-
-	FromY =:= ToY,
-	!.
-
-attack((FromX, FromY), (ToX, ToY)):-
-	(FromX + FromY) =:= (ToX + ToY),
-	!.
-
-attack((FromX, FromY), (ToX, ToY)):-
-	(FromX / FromY) =:= (ToX / ToY),
-	!.
+	abs(FromX - ToX) =:= abs(FromY - ToY).
